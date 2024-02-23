58598c069d5f55cb2bcddb041ca54c448fc1e94b
funnygeeker <funnygeeker@outlook.com>
2024-02-23 16:13:42 +0800

    Update the usage sample

--- main.py
+++ main.py
@@ -8,12 +8,11 @@ from lib.easydisplay import EasyDisplay
 # RP2040 & SSD1306
 i2c = busio.I2C(board.GP27, board.GP26)
 dp = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
-ed = EasyDisplay(display=dp, font="/text_lite_16px_2312.v3.bmf", show=True, color=1, clear=True,
-                 color_type="MONO", key=0)
+ed = EasyDisplay(display=dp, font="/text_lite_16px_2312.v3.bmf", show=True, clear=True, key=0)
 
 ed.bmp("/img/test.bmp", 0, 0)
 time.sleep(3)
-ed.pbm("/img/test.pbm", 0, 0, color_type="MONO")
+ed.pbm("/img/test.pbm", 0, 0)
 time.sleep(3)
 ed.text("你好，世界！\nHello World!\nこんにちは、世界！", 0, 0)
 
